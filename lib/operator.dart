8c000cec85cad8e32a75132f7eef83457ceb090e
zhoubaoqi <zhoubaoqi@icarbonx.com>
2018-11-16 18:08:25 +0800

    操作符和对象

--- lib/operator.dart
+++ lib/operator.dart
@@ -24,17 +24,6 @@ void operator() {
   print("operator(>>):${b >> 4 == 0x07}"); //右移4位
   print("operator(<<):${b << 4 == 0x790}"); //左移4位
 
-  const value = 0x22;
-  final bitmask = 0x0f;
-
-  assert((value & bitmask) == 0x02); // AND
-  assert((value & ~bitmask) == 0x20); // AND NOT
-
-  assert((value | bitmask) == 0x2f); // OR
-  assert((value ^ bitmask) == 0x2d); // XOR
-  assert((value << 4) == 0x220); // Shift left
-  assert((value >> 4) == 0x02); // Shift right
-
   //is
   var str = "zhangsan";
   print("operator(is):${str is String}");
--- lib/thread.dart
+++ lib/thread.dart
@@ -1,15 +1,41 @@
-
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
+///1.await关键字必须在async函数内部使用
+///2.调用async函数必须使用await关键字
+void main() {
+  setData();
+  futureFunction(); //  当所有任务完成后的回调函数
 
-main(){
-  getNetData().then((str) {
-    print(str);
-  });
-}
-Future<String> getNetData() async{
-  http.Response res = await http.get("http://www.baidu.com");
-  return res.body;
 }
 
+void futureFunction() {
+  var future = new Future(() => futureTask) //  异步任务的函数
+      .then((m) => "futueTask execute result:$m") //   任务执行完后的子任务
+      .then((m) => m.length) //  其中m为上个任务执行完后的返回的结果
+      .then((m) => printLength(m))
+      .whenComplete(() => whenTaskCompelete); //  当所有任务完成后的回调函数
+}
+
+int futureTask() {
+  return 22;
+}
+
+void printLength(int length) {
+  print("Text Length:$length");
+}
+
+void whenTaskCompelete() {
+  print("Task Complete");
+}
+
+getData() async {
+  //async关键字声明该函数内部有代码需要延迟执行
+  return await http.get(Uri.encodeFull("http://www.baidu.com"),
+      headers: {"Accept": "application/json"}); //await关键字声明运算为延迟执行，然后return运算结果
+}
+
+setData() async {
+  http.Response data = await getData();
+  print(data.body);
+}
