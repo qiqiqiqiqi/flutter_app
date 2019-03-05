a6fbcdcc881d29615d6e9c26f66b7db9fa047844
zhoubaoqi <zhoubaoqi@icarbonx.com>
2019-03-05 22:48:50 +0800

    动画

--- lib/view/scroll/customscrollview.dart
+++ lib/view/scroll/customscrollview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
+
 main() {
   runApp(MaterialApp(
     title: "CustomScrollView demo",
@@ -7,26 +8,72 @@ main() {
   ));
 }
 
-class CustomScrollViewDemo extends StatelessWidget {
+class CustomScrollViewDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CustomScrollViewState();
+  }
+}
+
+class CustomScrollViewState extends State<CustomScrollViewDemo> {
   GlobalKey bigCircleAvatarGlobalKey = GlobalKey();
   GlobalKey smallCircleAvatarGlobalKey = GlobalKey();
+  double barHeight = MediaQueryData.fromWindow(window).padding.top;
+  double titleHeight = kToolbarHeight;
+  double scale = 1.0;
+  double translationX = 0, translationY = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    MediaQueryData.fromWindow(window).padding.top;
+    // TODO: implement build
     return NotificationListener(
         onNotification: (ScrollNotification scrollNotification) {
-          double pixels = scrollNotification.metrics.pixels;
+          setState(() {
+            double pixels = scrollNotification.metrics.pixels;
 
-          RenderBox bigRenderBox =
-              bigCircleAvatarGlobalKey.currentContext.findRenderObject();
-          Offset bigLocalToGlobal = bigRenderBox.localToGlobal(Offset.zero);
+            RenderBox bigRenderBox =
+                bigCircleAvatarGlobalKey.currentContext.findRenderObject();
+            Offset bigLocalToGlobal = bigRenderBox.localToGlobal(Offset.zero);
 
-          RenderBox smallRenderBox =
-              smallCircleAvatarGlobalKey.currentContext.findRenderObject();
-          Offset smalllocalToGlobal = smallRenderBox.localToGlobal(Offset.zero);
-          print(
-              "CustomScrollViewDemo:pixels=$pixels,bigLocalToGlobal=${bigLocalToGlobal.toString()},bigRenderBox=${bigRenderBox.size.width},smalllocalToGlobal=${smalllocalToGlobal.toString()},smallRenderBox=${smallRenderBox.size.width}");
+            RenderBox smallRenderBox =
+                smallCircleAvatarGlobalKey.currentContext.findRenderObject();
+            Offset smalllocalToGlobal =
+                smallRenderBox.localToGlobal(Offset.zero);
+            double minScale =
+                smallRenderBox.size.width / bigRenderBox.size.width;
+            scale = 1 -
+                (pixels > (240 - barHeight - titleHeight)
+                        ? (240 - barHeight - titleHeight)
+                        : pixels) /
+                    (240 - barHeight - titleHeight);
+            scale = scale > minScale ? scale : minScale;
+            translationX = ((bigLocalToGlobal.dx +
+                        bigRenderBox.size.width / 2) -
+                    (smalllocalToGlobal.dx + smallRenderBox.size.width / 2)) *
+                (pixels > (240 - barHeight - titleHeight)
+                    ? (240 - barHeight - titleHeight)
+                    : pixels) /
+                (240 - barHeight - titleHeight);
+            translationY = ((bigLocalToGlobal.dy +
+                        bigRenderBox.size.height / 2) -
+                    (smalllocalToGlobal.dy + smallRenderBox.size.height / 2)) *
+                (pixels > (240 - barHeight - titleHeight)
+                    ? (240 - barHeight - titleHeight)
+                    : pixels) /
+                (240 - barHeight - titleHeight);
+            print(
+                "${(pixels > (240 - barHeight - titleHeight) ? (240 - barHeight - titleHeight) : pixels) / (240 - barHeight - titleHeight)},${MediaQuery.of(context).size.width}");
+            print(
+                "CustomScrollViewDemo:pixels=$pixels,bigLocalToGlobal=${bigLocalToGlobal.toString()},bigRenderBox=${bigRenderBox.size.width},smalllocalToGlobal=${smalllocalToGlobal.toString()},"
+                "smallRenderBox=${smallRenderBox.size.width},scale=$scale,translationX=$translationX,translationY=$translationY");
+          });
         },
         child: Stack(
           alignment: AlignmentDirectional.topCenter,
@@ -86,20 +133,30 @@ class CustomScrollViewDemo extends StatelessWidget {
                     itemExtent: 120)
               ],
             ),
-            SizedBox(
-              key: bigCircleAvatarGlobalKey,
-              width: 100,
-              height: 100,
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.blueGrey,
-                child: Image.asset(
-                  "images/ic_img_default.jpg",
-                  fit: BoxFit.fill,
-                ),
+            Positioned(
+              top: 70,
+              child: SizedBox(
+                key: bigCircleAvatarGlobalKey,
+                width: 100,
+                height: 80,
+                child: Transform(
+                    transform: Matrix4.translationValues(
+                        -translationX, -translationY, 0),
+                    child: Transform.scale(
+                      scale: scale,
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Colors.blueGrey,
+                        child: Image.asset(
+                          "images/ic_img_default.jpg",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    )),
               ),
             )
           ],
         ));
+    ;
   }
 }
