29c3613d1076d984f3d3f86a78bfdd62fa19c847
zhoubaoqi <zhoubaoqi@icarbonx.com>
2019-03-14 23:23:08 +0800

    动画

--- lib/custom/dragcard/PullDragContainer.dart
+++ lib/custom/dragcard/PullDragContainer.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
+class PullDragContiner extends StatefulWidget {
+  Widget headWidget, contentWidget;
+  double offsetY;
 
-class PullDragContiner extends StatefulWidget{
+  PullDragContiner({this.headWidget, this.contentWidget});
+
   @override
   State<StatefulWidget> createState() {
-    return null;
+    return PullDragState();
   }
-
 }
 
-class PullDragState extends State<PullDragContiner>{
+class PullDragState extends State<PullDragContiner> {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Stack(
+      children: <Widget>[
+        Positioned(
+            top: null,
+            height: 200,
+            bottom: widget.offsetY,
+            left: 0,
+            right: 0,
+            child: widget.headWidget),
+        Positioned(
+            bottom: -widget.offsetY,
+            top: widget.offsetY,
+            left: 0,
+            right: 0,
+            child: widget.contentWidget)
+      ],
+    );
   }
-
 }
