62d9b780de58aecb4fa0eb8242d92d198e51f144
zhoubaoqi <zhoubaoqi@icarbonx.com>
2019-04-11 19:46:38 +0800

    启动页

--- lib/fitforce/fitforce_splash.dart
+++ lib/fitforce/fitforce_splash.dart
@@ -11,15 +11,12 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
-  Animation _animation;
 
   void initState() {
     super.initState();
     _controller = AnimationController(
         vsync: this, duration: Duration(milliseconds: 3000));
-    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
-
-    _animation.addStatusListener((status) {
+    _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         Navigator.of(context).pushAndRemoveUntil(
             MaterialPageRoute(builder: (context) => MyHomePage()),
@@ -38,15 +35,7 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   Widget build(BuildContext context) {
-    return FadeTransition(
-      //透明度动画组件
-      opacity: _animation, //执行动画
-      child: Image.asset(
-          //网络图片
-          'images/bg_guide_page2.png',
-          fit: BoxFit.cover // 充满父容器
-          ),
-    );
+    return Image.asset('images/launcher_image.png', fit: BoxFit.cover);
   }
 }
 
